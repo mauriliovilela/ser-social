918e0d465b2ef9eb3972a02e8e1f70df7390d78d
eduardo.vfoliveira <eduardo.vfoliveira@gmail.com>
2012-05-10 18:25:59 +0000

    Subindo  Projeto

---
+++ RedeSerSocial/cadastro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head> 
+        <meta charset="utf-8"/>
+        <title>Rede Ser Social</title>
+        <link rel="stylesheet" href="estilos/cadastro.css" type="text/css"/>
+    </head>  
+    <body>
+
+    </body>
+
+
+
+
+
+</html>
