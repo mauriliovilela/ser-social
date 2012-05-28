87836db23940fc34160b9b59ebac7c5c411c307f
victor.hugoegs <victor.hugoegs@gmail.com>
2012-05-28 16:48:23 +0000

    Editando as validações.

---
+++ RedeSerSocial/js/validacoes.js
@@ -0,0 +1,10 @@
+function somente_numero(campo){  
+    var digits="0123456789"  
+    var campo_temp   
+    for (var i=0;i<campo.value.length;i++){  
+        campo_temp=campo.value.substring(i,i+1)   
+        if (digits.indexOf(campo_temp)==-1){  
+            campo.value = campo.value.substring(0,i);  
+        }  
+    }  
+}
