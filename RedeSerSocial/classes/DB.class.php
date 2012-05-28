a97abfd95c7f335c04fb41ea378423278d76efee
eduardo.vfoliveira <eduardo.vfoliveira@gmail.com>
2012-05-28 18:20:13 +0000

    Inclusão e correção dos arquivos.

--- RedeSerSocial/classes/DB.class.php
+++ RedeSerSocial/classes/DB.class.php
@@ -1,19 +1,36 @@
 <?php
-
-class DB {
-
-    private static $conn;
-
-    static function getConn() {
-        //Se a variável $conn for vazia ela cria a instância.
-        if (is_null(self::$conn)) {
-            //Estamos usando conexão PDO para quando quiser mudar o banco futuramente, não ter problemas.
-            //PDO é uma classe do PHP onde temos o dns, usuário , senha.
-            self::$conn = new PDO('mysql:host=localhost;dbname=sersocial', 'root', 'user00');
-        }
-        return self::$conn;
-    }
-
-}
-
-?>
+	class DB{
+		private static $conn;
+		static function getConn(){
+			if(is_null(self::$conn)){
+				self::$conn = new PDO('mysql:host=localhost;dbname=ser','root','');
+				self::$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+			}
+			return self::$conn;
+		}
+	}
+	
+	function logErros($errno){
+		
+		if(error_reporting()==0) return;
+		
+		$exec = func_get_arg(0);
+		
+		$errno = $exec->getCode();
+		$errstr = $exec->getMessage();
+		$errfile = $exec->getFile();
+		$errline = $exec->getLine();
+		$err = 'CAUGHT EXCEPTION';
+		
+		if(ini_get('log_errrors')) error_log(sprintf("PHP %s: %s in %s on line %d",$err,$errstr,$errfile,$errline));
+		
+		$strErro = 'erro: '.$err.' no arquivo: '.$errfile.' ( linha '.$errline.' ) :: IP('.$_SERVER['REMOTE_ADDR'].') data:'.date('d/m/y H:i:s')."\n";
+		
+		
+		$arquivo = fopen('logerro.txt','a');
+		fwrite($arquivo,$strErro);
+		fclose($arquivo);
+		
+		set_error_handler('logErros');
+		
+	}
