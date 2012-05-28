46f81c2470f442776caad0c6646d40647e0b0841
eduardo.vfoliveira@gmail.com <eduardo.vfoliveira@gmail.com@b29c93b4-2b6d-4525-c276-559631b41cd5>
2012-05-28 17:21:10 +0000

    Usando as validações com expressões regulares.

--- trunk/RedeSerSocial/cadastro.php
+++ trunk/RedeSerSocial/cadastro.php
@@ -6,13 +6,14 @@
         <title>Rede Ser Social - Cadastro</title>
         <link rel="stylesheet" href="estilos/cadastro.css" type="text/css"/>
         <script type="text/javascript" src="js/validacoes.js"></script>
+
     </head>
 
     <body>
         <!--Início do Topo-->
         <div id="topo">
             <div class="cAlign">
-                <a href="http://www.joaquimnabuco.edu.br/" > <img src="images/logo.png" alt="Ser Social"> </a>  <!--<span><a href="http://www.joaquimnabuco.edu.br/" >Joaquim Nabuco</a> </span><span><a href="http://www.joaquimnabuco.edu.br/" >Facebook</a> </span> <span><a href="http://www.mauriciodenassau.edu.br/" >Faculdade Maurício de Nassau</a> </span>--></div>
+            <a href="http://www.joaquimnabuco.edu.br/" > <img src="images/logo.png" alt="Ser Social"> </a>  <!--<span><a href="http://www.joaquimnabuco.edu.br/" >Joaquim Nabuco</a> </span><span><a href="http://www.joaquimnabuco.edu.br/" >Facebook</a> </span> <span><a href="http://www.mauriciodenassau.edu.br/" >Faculdade Maurício de Nassau</a> </span>--></div>
         </div>
         <!--Fim do Topo-->
 
@@ -25,68 +26,85 @@
                 <!-- Início do Formulário-->
                 <div id="formulario">
                     <?php
-                    if (isset($_SERVER['REQUEST_METHOD']) AND $_SERVER['REQUEST_METHOD'] == 'POST')
+                    if (isset($_SERVER['REQUEST_METHOD']) AND $_SERVER['REQUEST_METHOD'] == 'POST') {
+
                         extract($_POST);
-                    //Mudar essas verificações (fazer com javascript)
-                    if ($nome == '' or strlen($nome) < 3) {
-                        echo 'Digite seu nome corretamente!';
-                    } elseif ($sobrenome == '' or strlen($sobrenome) < 5) {
-                        echo 'Digite seu sobrenome corretamente!';
-                    } elseif ($email == '') {
-                        echo 'Digite seu e-mail!';
-                    } elseif (!preg_match("/^[a-z0-9_\.\-]+@[a-z0-9_\.\-]*[a-z0-9_\-]+\.[a-z]{2,4}$/i", $email)) {
-                        echo 'Digite um e-mail válido!';
-                    } else {
 
+                        echo '<h3>';
 
+                        if ($nome == '' OR strlen($nome) < 4) {
+                            echo 'Escreva seu nome corretamente';
+                        } elseif ($sobrenome == '' OR strlen($sobrenome) < 4) {
+                            echo 'Escreva seu sobrenome corretamente';
+                        } elseif ($matricula == '' OR strlen($matricula) < 8) {
+                            echo 'Digite corretamente a sua matricula';
+                        } elseif (strlen($matricula) > 8) {
+                            echo 'A sua matrícula não pode ser maior que 8 digitos!';
+                        } elseif ($email == '') {
+                            echo 'Escreva seu e-mail';
+                        } elseif (!preg_match("/^[a-z0-9_\.\-]+@[a-z0-9_\.\-]*[a-z0-9_\-]+\.[a-z]{2,4}$/i", $email)) {
+                            echo 'Este e-mail é invalido';
+                        } else {
 
-                        include ('classes/DB.class.php');
-                        $verificar = DB::getConn()->prepare("SELECT `id` FROM `usuarios` WHERE `email`=?");
-                        if ($verificar->execute(array($email))) {
-                            if ($verificar->rowCount() >= 1) {
-                                echo 'Este e-mail já está cadastrado em nosso sistema';
-                            } elseif ($senha == '' OR strlen($senha) < 4) {
-                                echo 'Digite sua senha, ela tem de ter mais de 4 caracteres';
-                            }
-                            //Consertar o Captcha
-                            // elseif (strtolower($captcha) <> strtolower($_SESSION['captchaCadastro'])) {
-                            //    echo 'O código digitado não corresponde a imagem! ';
-                            //   echo $captcha;
-                            //}
-                            else {
-                                $senhaInsert = sha1($senha);
-                                $nascimento = "$ano-$mes-$dia";
-                                $inserir = DB::getConn()->prepare("INSERT INTO`usuarios` SET `email`=?,`senha`=?, `nome`=?, `sobrenome`=?,`sexo`=?,`nascimento`=?, `cadastro`=NOW()");
-                                if ($inserir->execute(array($email, $senhaInsert, $nome, $sobrenome, $sexo, $nascimento))) {
-                                    header('Location: ./');
+                            include('classes/DB.class.php');
+
+                            try {
+
+                                $verificar = DB::getConn()->prepare("SELECT `id` FROM `usuarios` WHERE `email`=?");
+                                if ($verificar->execute(array($email))) {
+                                    if ($verificar->rowCount() >= 1) {
+                                        echo 'Este e-mail ja esta cadastrado em nosso sistema';
+                                    } elseif ($senha == '' OR strlen($senha) < 4) {
+                                        echo 'Digite sua senha, ela tem de ter mais de 4 caracteres';
+                                    }
+                                    //elseif(strtolower($captcha) <> strtolower($_SESSION['captchaCadastro'])){
+                                    //echo 'O código digitado no captcha não corresponde com a imagem';
+                                    //}
+                                    else {
+                                        $senhaInsert = sha1($senha);
+                                        $nascimento = "$ano-$mes-$dia";
+
+                                        $inserir = DB::getConn()->prepare("INSERT INTO `usuarios` SET `email`=?, `senha`=?, `nome`=?, `matricula`=?,`sobrenome`=?, `sexo`=?, `nascimento`=?, `cadastro`=NOW()");
+
+                                        if ($inserir->execute(array($email, $senhaInsert, $nome, $matricula, $sobrenome, $sexo, $nascimento))) {
+                                            header('Location: ./');
+                                        }
+                                    }
                                 }
+                            } catch (PDOException $e) {
+                                echo 'Sistema indisponível';
+                                logErros($e);
                             }
                         }
+                        echo '</h3>';
                     }
                     ?>
                     <form name="cadastro" method="post" action="">
                         <div>
                             <div class="inputFloat">
-                                <span>Nome</span>
+                                <span>Nome *</span>
                                 <input type="text" name="nome" class="inputTxt" value="<?php if (isset($nome)) echo $nome; ?>"/>                                
                             </div>
 
                             <div class="inputFloat">
-                                <span>Sobrenome</span>
+                                <span>Sobrenome *</span>
                                 <input type="text" name="sobrenome" class="inputTxt"value="<?php if (isset($sobrenome)) echo $sobrenome; ?>"/>                                
                             </div>
 
                             <div class="inputFloat">
-                                <span>Matrícula</span>
-                                <input type="text" name="matricula" class="inputTxt" onKeyUp="javascript:somente_numero(this);" value="<?php if (isset($matricula)) echo $matricula; ?>"/>                                
+                                <span>Matrícula *</span>
+                                <input type="text" name="matricula" class="inputTxt" id="inumeros" onkeypress="mascara(this,soNumeros)" value="<?php if (isset($matricula)) echo $matricula; ?>"/>                                
                             </div>
 
                         </div>
+                        
+
                         <span class="spanHide">Eu sou</span>
                         <select name="sexo">
                             <option <?php if ($sexo == 'homem') echo 'selected="selected"'; ?> value="homem">Homem&nbsp;</option>
                             <option <?php if ($sexo == 'mulher') echo 'selected="selected"'; ?>value="mulher">Mulher&nbsp;</option>
                             <option <?php if ($sexo == 'travesti') echo 'selected="selected"'; ?>value="travesti">Travesti&nbsp;</option>
+                            <option <?php if ($sexo == 'viado') echo 'selected="selected"'; ?>value="viado">Viado&nbsp;</option>
                             <option <?php if ($sexo == 'sapatao') echo 'selected="selected"'; ?>value="sapatao">Sapatão&nbsp;</option>
                         </select>
 
@@ -145,18 +163,20 @@
                         <span class="spanHide">Senha</span>
                         <input type="password" name="senha" class="inputTxt" value="<?php echo $senha; ?>"/>
 
-                        <span class="spanHide">Verificação contra fraudes</span>
-                        <div>
-                            <div class="captchaFloat"><img src="captcha/captcha.php" width="200" height="60" alt="Captcha">
-                            </div>
+                                <!-- <span class="spanHide">Verificação contra fraudes</span>
+                                <div>
+                                <div class="captchaFloat"><img src="captcha/captcha.php" width="200" height="60" alt="Captcha">
+                                </div>
 
-                            <div class="inputFloat">
+                                <div class="inputFloat">
                                 <span>Digite os caracteres</span>
                                 <input type="text" name="captcha" class="inputTxt"/>
-                            </div>
-                        </div>
+                                        </div>
+                                        </div>-->
                         <span>&nbsp;</span>
                         <input type="submit" value="" class="submitCadastro" name="Cadastrar"/>
+                        <span class="campos">* Campos obrigatórios</span><br/><br/>
+                        
                     </form>
                 </div><!-- Fim do Formulário-->
 
@@ -165,8 +185,7 @@
         </div><!--Fim do cAlign-->
 
         <!--Início do Rodapé-->
-       <?php include('includes/footer.php'); ?>
-       <!--Fim do Rodapé-->
+        <?php include('includes/footer.php'); ?><!--Fim do Rodapé-->
     </body>
 
 </html>
--- trunk/RedeSerSocial/js/validacoes.js
+++ trunk/RedeSerSocial/js/validacoes.js
@@ -1,10 +1,25 @@
-function somente_numero(campo){  
-    var digits="0123456789"  
-    var campo_temp   
-    for (var i=0;i<campo.value.length;i++){  
-        campo_temp=campo.value.substring(i,i+1)   
-        if (digits.indexOf(campo_temp)==-1){  
-            campo.value = campo.value.substring(0,i);  
-        }  
-    }  
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+
+function leech(v){
+    v=v.replace(/o/gi,"0")
+    v=v.replace(/i/gi,"1")
+    v=v.replace(/z/gi,"2")
+    v=v.replace(/e/gi,"3")
+    v=v.replace(/a/gi,"4")
+    v=v.replace(/s/gi,"5")
+    v=v.replace(/t/gi,"7")
+    return v
+}
+
+
+function soNumeros(v){
+    return v.replace(/\D/g,"")
 }
