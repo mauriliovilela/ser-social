d62ed28b6e3237f3f808fc18052fb256c55542c0
eduardo.vfoliveira@gmail.com <eduardo.vfoliveira@gmail.com@b29c93b4-2b6d-4525-c276-559631b41cd5>
2012-06-06 18:11:37 +0000

    Lista de amigos mais organizada (usando class).

--- trunk/RedeSerSocial/includes/amigos.php
+++ trunk/RedeSerSocial/includes/amigos.php
@@ -1,23 +1,15 @@
 <div class="blocos" id="meus-amigos">
 
-    <?php
-    $selAmigos = DB::getConn()->prepare('SELECT u.id, u.nome, u.sobrenome, u.imagem FROM usuarios u 
-									INNER JOIN amizade a ON (((u.id=a.de) AND (a.para=?)) OR ((u.id=a.para) AND (a.de=?))) AND a.status=1');
+    <?php $list_amigos = Amizade::list_amigos($idExtrangeiro); ?>
 
-    $selAmigos->execute(array($idExtrangeiro, $idExtrangeiro));
-
-    $numamigos = $selAmigos->rowCount();
-    ?>
-
-    <span>Meus amigos(<?php echo $numamigos ?>) <a href="#">Todos</a></span>
+    <span>Meus amigos(<?php echo $list_amigos['num'] ?>) <a href="#">Todos</a></span>
     <ul>
         <?php
-        if ($numamigos > 0) {
+        if ($list_amigos['num'] > 0) {
 
-            while ($resAmigos = $selAmigos->fetch(PDO::FETCH_NUM)) {
+            foreach ($list_amigos['dados'] as $resAmigos) {
 
-               
-                echo '<li><a href="perfil.php?uid=' . $resAmigos[0] . '"><img width="50" height="50" src="uploads/usuarios/' . user_img($resAmigos[3]). '" alt="" title="' . $resAmigos[1] . ' ' . $resAmigos[2] . '" /></a></li>';
+                echo '<li><a href="perfil.php?uid=' . $resAmigos[0] . '"><img width="50" height="50" src="uploads/usuarios/' . user_img($resAmigos[3]) . '" alt="" title="' . $resAmigos[1] . ' ' . $resAmigos[2] . '" /></a></li>';
             }
         } else {
             echo 'Você não tem amigos';
